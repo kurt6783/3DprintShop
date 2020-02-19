56dd4794bb53e9e7dfcca380209899efbe23d7f5
kurt6783 <k0911245920@gmail.com>
2020-02-19 11:42:37 +0800

    新增views->header/footer/home

---
+++ application/views/header/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>3DprintShop</title>
+	<link rel="icon" type="image/x-icon" href="https://s2.coinmarketcap.com/static/img/coins/200x200/2099.png?_=cb31027">		
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="padding-top:50px">		
+	<h1><?php echo $title ?></h1>
+	<div>
+		<nav class="navbar-fixed-top"> 
+            <a class="btn" href="http://127.0.0.1/3DprintShop/Home">Home</a>          
+            <a class="btn" href="http://127.0.0.1/3DprintShop/Register">Register</a>        
+    	</nav>
+	</div>
+	
