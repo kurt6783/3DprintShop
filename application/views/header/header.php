cfe81b4b2fa4926f22f14b2ee605081c1e9e5cdd
kurt6783 <k0911245920@gmail.com>
2020-02-20 19:29:34 +0800

    將bootstrap套入header

--- application/views/header/header.php
+++ application/views/header/header.php
@@ -3,14 +3,27 @@
 <head>
 	<title>3DprintShop</title>
 	<link rel="icon" type="image/x-icon" href="https://s2.coinmarketcap.com/static/img/coins/200x200/2099.png?_=cb31027">		
-	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 </head>
-<body style="padding-top:50px">		
-	<h1><?php echo $title ?></h1>
-	<div>
+<body>		
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="http://127.0.0.1/3DprintShop">Home</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<!-- <li><a href="http://127.0.0.1/3DprintShop/#">Register</a></li>				 -->
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">SighIn</a></li>
+				<li><a href="#">Register</a></li>
+			</ul>
+		</div>		
+	</nav>
+	<!-- <div>
 		<nav class="navbar-fixed-top"> 
-            <a class="btn" href="http://127.0.0.1/3DprintShop/Home">Home</a>          
+            <a class="btn" href="http://127.0.0.1/3DprintShop">Home</a>          
             <a class="btn" href="http://127.0.0.1/3DprintShop/Register">Register</a>        
     	</nav>
-	</div>
+	</div> -->
 	
