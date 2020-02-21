b1ed917be4559d13a269346ee20304814a23b834
kurt6783 <k0911245920@gmail.com>
2020-02-21 14:41:54 +0800

    新增註冊成功寫入DB功能

--- application/views/register/register.php
+++ application/views/register/register.php
@@ -5,8 +5,8 @@
 	    <input type="name" class="form-control" name="name" placeholder="Eric">	    
   	</div>
   	<div class="form-group">
-	    <label for="identity_card">Identity_card</label>
-	    <input type="name" class="form-control" name="identity_card" placeholder="A123456789">    
+	    <label for="identityCard">IdentityCard</label>
+	    <input type="name" class="form-control" name="identityCard" placeholder="A123456789">    
   	</div>
 	<div class="form-group">
 	    <label for="email">Email address</label>
@@ -22,12 +22,16 @@
 	</div>
 	<div class="form-group">
 		<label for="password">PasswordConfirm</label>
-	    <input type="password" class="form-control" name="PasswordConfirm"placeholder="at least 8 char">
+	    <input type="password" class="form-control" name="passwordConfirm" placeholder="at least 8 char">
 	</div>
 	<div class="form-group">
 		<label for="phoneNumber">Phone number</label>
 	    <input class="form-control" name="phoneNumber" placeholder="09xxxxxxxx">
-	</div>	  	 	  	  
+	</div>
+	<div class="form-group">
+		<label for="birthday">birthday</label>
+	    <input class="form-control" name="birthday" placeholder="1992-09-29">
+	</div>	  	  	 	  	  
   	<button type="submit" class="btn btn-primary">Submit</button>
 </div>
 </form>
