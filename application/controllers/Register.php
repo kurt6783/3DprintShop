0e3292bc433ef92cb273b48e73b4f263a64e9c15
kurt6783 <k0911245920@gmail.com>
2020-02-21 15:12:18 +0800

    新增註冊功能

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -2,7 +2,8 @@
 class Register extends CI_Controller{
 
 	public function __construct(){
-        parent::__construct();        
+        parent::__construct(); 
+        $this->load->model('Register_model');
         $this->load->helper('url');
     }
 
@@ -14,8 +15,57 @@ class Register extends CI_Controller{
 		$this->load->view('footer/footer');		
 	}	
 
-	public function create(){
-		$this->load->view('header/header', $data);
-		$this->load->view('footer/footer');		
+	public function create(){	
+		$data['title'] = 'Register result';	
+
+		//測試區
+		//$data['result'] = $this->Register_model->show_input();	
+		// $this->load->view('header/header', $data);			
+		// //$this->load->view('register/show', $data);
+		// //$this->load->view('footer/footer');
+		// $this->load->helper('date');
+		// // echo unix_to_human(time());
+		// echo unix_to_human(now('Asia/Taipei'),true,'us');
+		// echo unix_to_human(now('Asia/Taipei'),true,'eu');
+		// return ;
+		//測試區
+
+		$isDataComplete = $this->Register_model->check_isDataComplete();
+		if($isDataComplete==false){
+			$data['result'] = 'data is not complete';
+			$this->load->view('header/header', $data);			
+			$this->load->view('register/verification', $data);
+			$this->load->view('footer/footer');
+			return;
+		}
+		$isUserExist = $this->Register_model->get_isUserExist();	
+		if($isUserExist==true){
+			$data['result'] = 'this account had been used';
+			$this->load->view('header/header', $data);			
+			$this->load->view('register/verification', $data);
+			$this->load->view('footer/footer');
+			return;
+		}
+		$isPasswordLegal = $this->Register_model->check_isPasswordLegal();
+		if($isPasswordLegal==false){
+			$data['result'] = 'your password is not legal';
+			$this->load->view('header/header', $data);			
+			$this->load->view('register/verification', $data);
+			$this->load->view('footer/footer');
+			return;
+		}
+		$createUser = $this->Register_model->set_createUser();
+		if($createUser==true){
+			$data['result'] = 'Register success. Please check your phone to activate your account';
+			$data['url'] = 
+			$this->load->view('header/header', $data);
+			$this->load->view('register/verification', $data);
+			$this->load->view('footer/footer');
+		}else{
+			$data['result'] = 'Register failed';
+			$this->load->view('header/header', $data);
+			$this->load->view('register/verification', $data);
+			$this->load->view('footer/footer');
+		}		
 	}	
 }
---
+++ application/models/Register_model.php
@@ -0,0 +1,67 @@
+<?php
+class Register_model extends CI_Model{
+
+	public function __construct(){
+		$this->load->database();
+		$this->load->helper('url');
+		$this->load->helper('date');
+	}
+
+	public function show_input(){
+		$data = $this->input->post();
+		return $data;
+	}
+
+	public function set_createUser(){
+		$this->load->helper('security');
+		$this->load->helper('string');
+		$data = array(
+			'name' => $this->input->post('name'),
+			'identityCard' => $this->input->post('identityCard'),
+			'email' => $this->input->post('email'),
+			'account' => $this->input->post('account'),
+			'password' => do_hash($this->input->post('password'),'MD5'),
+			'phoneNumber' => $this->input->post('phoneNumber'),
+			'verificationCode'=> random_string('alnum',20),
+			'level' => 1,
+			'birthday' => $this->input->post('birthday'),
+			'setDate' => unix_to_human(now('Asia/Taipei'),true,'eu'),
+		);
+		return $this->db->insert('users',$data);
+	}
+
+	public function get_isUserExist(){
+		$data = array('account' => $this->input->post('account'));
+		$query = $this->db->get_where('users',$data)->result_array();
+		if(count($query)==0){
+			return false;
+		}else{
+			return true;
+		}
+	}
+
+	public function get_verificationCodeByAccount(){
+		$data = array()
+		$data = array()
+	}
+
+	public function check_isDataComplete(){
+		$datas=$this->input->post();
+		foreach($datas as $data){
+			if(strlen($data)==0){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public function check_isPasswordLegal(){
+		$password = $this->input->post('password');
+		$passwordConfirm = $this->input->post('passwordConfirm');
+		if($password==$passwordConfirm){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
