93e8fef15fae85c61cb9be11203b5ab9d0f521c5
kurt6783 <k0911245920@gmail.com>
2020-03-19 15:36:25 +0800

    串接Igmur API

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -3,6 +3,7 @@
 		<h1><?php echo $title ?></h1>
 	</div>
 </div>
+
 <div class="container">
 	<div class="form-group">
 		<ul>
@@ -11,12 +12,26 @@
 	</div>
 </div>
 
+<div>
+	<form action="Imgur/test2" method="POST" enctype="multipart/form-data"> 
+		<input type="file" name="pictureURL">
+		<input type="text" name="pictureName">
+		<input type="text" name="pictureDescription">
+		<button type="submit">submit</button>
+	</form>	
+</div>
+
+<!-- <img src="https://imgur.com/tCaFcMl"> -->
+	<!-- <img src="https://i.imgur.com/tCaFcMl.jpg"> -->
+
+
 
 <div >
 	<h2 id="div1">使用 jQuery AJAX 修改文本内容</h2>
 	<button id = "submit">Submit</button>
 </div >
 
+<div>
 <script>	
 	$("#submit").click(function(){
 		$.ajax({url:"AJAX/test",
