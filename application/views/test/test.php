403a143db683d4b87895a74e555f3b68acabee4f
kurt6783 <k0911245920@gmail.com>
2020-04-10 23:53:11 +0800

    新增測試功能說明

--- application/views/test/test.php
+++ application/views/test/test.php
@@ -7,6 +7,7 @@
 <div class="container">
 	<div class="page-header">
 		<h1>picture practice</h1>
+		<p>串接imgur圖床API完成上傳圖片功能</p>
 		<form action="Home/picture" method="POST" enctype="multipart/form-data">
 			<div class="form-group">				
 				<input type="file" name="pictureURL">
@@ -27,6 +28,7 @@
 </div>
 
 <div class="container">
+	<p>串接imgur圖床API完成下載並顯示圖片功能</p>
 	<div class="page-header">		
 		<?php foreach (json_decode($pictures,true)['data'] as $picture ) { ?>
 				<img src="<?php echo $picture['link'] ?>" width="30%" height="30%">
@@ -39,6 +41,7 @@
 <div class="container">
 	<div class="page-header">
 		<h1>AJAX practice</h1>
+		<p>利用AJAX向後端隨機取0-100數字</p>
 		<h2 id="div1">AJAX</h2>
 		<button id = "submit">Submit</button>
 		<script>	
